e3ff4dccef8e8c6e5c1ef7697a550042ca973a46
Gregg CESARINE <gcesarine@rennes.sii.fr>
2014-10-28 15:19:50 +0100

    Fix subdomain with wildcard

--- Resources/public/js/ng-search/ng-search.js
+++ Resources/public/js/ng-search/ng-search.js
@@ -9,8 +9,6 @@ var searchApp = angular.module('searchApp', [
 searchApp.config(function($sceDelegateProvider) {
 	$sceDelegateProvider.resourceUrlWhitelist([
 		// Allow same origin resource loads.
-		'self',
-		// Allow loading from our assets domain.  Notice the difference between * and **.
-		'http://static*.solerni.localhost/**'
+		'**'
 	]);
 });
